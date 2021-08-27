29aa4ee8a77a15bd4a5afdbf4882f70429965bdb
Franpanozzo <francisco.panozzosf@gmail.com>
2021-08-27 14:26:44 -0300

    test autoclases

--- spec/guerra_spec.rb
+++ spec/guerra_spec.rb
@@ -12,10 +12,30 @@ describe 'golondrinas' do
     Muralla.new
   }
 
+  let(:una_muralla2) {
+    Muralla.new
+  }
+
   it 'un espadachin ataca una muralla y esta pierde energia' do
     un_espadachin.atacar(una_muralla)
     expect(una_muralla.energia).to eq(180)
   end
 
+  it 'test sobre autometodo en muralla' do
+    class << una_muralla
+      def deteriorar
+        self.energia -= 10
+      end
+    end
+
+    una_muralla.deteriorar()
+    expect(una_muralla.energia).to eq(190)
+  end
+
+  it 'testeo atributos de instancia de clase por accesors' do
+    un_espadachin.atacar(una_muralla)
+    expect(una_muralla.energia).to eq(180)
+    expect(una_muralla2.energia).to eq(200)
+  end
 
 end
