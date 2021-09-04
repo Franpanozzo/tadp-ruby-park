8bded35d3ebe0308f36dc9f588c8123d648fcf21
Franpanozzo <francisco.panozzosf@gmail.com>
2021-09-04 02:18:22 -0300

    scripts clase 2 con tests

---
+++ main/spec/guerra2_spec.rb
@@ -0,0 +1,35 @@
+require 'rspec'
+require_relative '../src/Guerra2'
+
+describe 'golondrinas' do
+
+  let(:un_guerrero) {
+    Guerrero.new
+  }
+
+  let(:un_guerrero2) {
+    Guerrero.new
+  }
+
+  it 'un guerrero puede descansar como defensor y atacante' do
+    un_guerrero.descansar
+    expect(un_guerrero.energia).to eq(110)
+    expect(un_guerrero.descansado).to eq(true)
+  end
+
+  it 'atacan a un peloton cobarde y se retira' do
+    un_peloton = Peloton.cobarde([un_guerrero,un_guerrero2])
+    un_guerrero.lastimado()
+    expect(un_peloton.retirado).to eq(true)
+  end
+
+  it 'atacan a un peloton cobarde y se retira' do
+    un_guerrero.energia = 20
+    un_peloton = Peloton.descansador([un_guerrero,un_guerrero2])
+    un_guerrero.lastimado()
+    expect(un_guerrero.energia).to eq(30)
+    expect(un_guerrero2.energia).to eq(100)
+  end
+
+
+end
